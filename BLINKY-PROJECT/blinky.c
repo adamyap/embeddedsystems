da4678cda3d70a620ac33f8a55f8df72e78c9fd9
Ali-Bristol <98831232+Ali-Bristol@users.noreply.github.com>
2023-10-09 10:47:46 +0100

    fix blinky.c

--- BLINKY-PROJECT/blinky.c
+++ BLINKY-PROJECT/blinky.c
@@ -1,10 +1,14 @@
-#include <msp430.h>
+#include <msp430fr2433.h> 
 int main(void) {
  WDTCTL = WDTPW | WDTHOLD; // Stop watchdog timer
-P1DIR |= 0x01; // Set P1.0 as output
+// Disable the GPIO power-on default high-impedance mode
+// to activate previously configured port settings
+PM5CTL0 &= ~LOCKLPM5; //off
+P1DIR |= BIT0; // Set P1.0 as output
  for(;;) {
 volatile unsigned int i; // prevent optimization
-P1OUT ^= 0x01; // Toggle P1.0 using XOR
+P1OUT ^= BIT0; // Toggle P1.0 using XOR
+ 
  i = 20000; // SW Delay
  do
  i--;
