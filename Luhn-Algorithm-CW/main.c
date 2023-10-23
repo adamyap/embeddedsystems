d63ee8373e5b78dcab4e272b42c3555737651d4e
Adam Yap <98854485+adamyap@users.noreply.github.com>
2023-10-23 18:53:28 +0100

    Create main.c

---
+++ Luhn-Algorithm-CW/main.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <msp430.h>
+#include <stdbool.h>
+
+volatile int n;
+int Num[16] = {};
+
+bool luhn_check (int number[])
+{
+    int numDigit = 0;
+    int numSum = 0;
+;
+    int i;
+    for (i=15;i >= 0; i--)
+    {
+        /* Convert digit to integer */
+        numDigit = number [i];
+
+        /* Deal with the digit here */
+
+        /* Double every other digit */
+        if (i % 2 == 0)
+        {
+            numDigit = numDigit * 2;
+
+            /* If doubled digit is ten or higher, sum its digits */
+            if (numDigit > 9)
+            {
+                /* Doubled digit will never be greater than 18, so modulo 10 plus one */
+                numDigit = (numDigit % 10) + 1;
+            }
+        }
+
+        /* Update sum */
+        numSum = numSum + numDigit;
+    }
+
+    /* Function output */
+    return (numSum % 10 == 0);
+}
+
+void sendString(char *str)
+{
+    volatile int length = strlen(str);
+    unsigned int i;
+    for (i=0; i <= length; i++)
+    {
+        while (!(UCA0IFG & UCTXIFG)); // Wait for TX buffer to be ready for new data
+        UCA0TXBUF = str[i];
+        volatile int i;
+        for(i=0;i<10000;i++); // delay
+    }
+}
+
+int main(void)
+{
+
+    /* UART Initialisation */
+
+    WDTCTL = WDTPW | WDTHOLD;                 // Stop watchdog timer
+    PM5CTL0 &= ~LOCKLPM5;
+
+    P1DIR |= BIT0 | BIT1;
+    P1OUT &= ~(BIT0 | BIT1);
+
+
+    // Configure UART pins
+    P1SEL0 |= BIT4 | BIT5;                    // set 2-UART pin as second function
+
+    // Configure UART for Power UP default clock rate
+    UCA0CTLW0 |= UCSWRST;                     // Put eUSCI in reset
+    UCA0CTLW0 |= UCSSEL__SMCLK;               // set equal to SMCLK
+
+    // Baud Rate calculation 115200 baud
+    UCA0BR0 = 8;                           // 1000000/115200 = 8.68
+    UCA0MCTLW = 0xD600;                    // Remainder - 0.68  Table 21.4 & 21.5
+
+    UCA0CTLW0 &= ~UCSWRST;                    // Initialize eUSCI
+    UCA0IE |= UCRXIE;                         // Enable USCI_A0 RX interrupt
+
+    __bis_SR_register(LPM0_bits|GIE);     // Enter LPM0 CPU off, SMCLK running
+    __no_operation();                     // For debugger
+}
+
+
+#pragma vector = USCI_A0_VECTOR
+__interrupt void USCI_A0_ISR(void)
+{
+    /* Variables */
+    volatile int i;
+    volatile int d;
+    switch(UCA0IV)
+        {
+            case USCI_NONE: break;
+            case USCI_UART_UCRXIFG:
+                while(!(UCA0IFG & UCTXIFG)); // Wait for TX buffer to be ready for new data
+                UCA0TXBUF = UCA0RXBUF;
+                char input = UCA0RXBUF;
+                if (isdigit(input))
+                {
+                    Num[n] = input - '0';
+                    n++;
+
+                    if (n >= 16)
+                    {
+                        n = 0;
+                        if (luhn_check(Num))
+                        {
+                            P1OUT ^= BIT1;
+                            TA0CCR0 = 16384;
+                            TA0CTL = MC_1 | ID_0 | TASSEL_1 |TACLR;
+                            for(;;)
+                            {
+                                while(!(TA0CTL & TAIFG)){}
+                                TA0CTL = MC_0 | ID_0 | TASSEL_1 |TACLR;
+                                TA0CTL &= ~TAIFG;
+                                TA0CTL |= TACLR;
+                                P1OUT ^=BIT1;
+                                break;
+                            }
+                            sendString("\n\rValid\n\r");
+                        }
+                        else
+                        {
+                            P1OUT ^= BIT0;
+                            TA0CCR0 = 32768;
+                            TA0CTL = MC_1 | ID_0 | TASSEL_1 |TACLR;
+                            for(;;)
+                            {
+                                while(!(TA0CTL & TAIFG)){}
+                                TA0CTL = MC_0 | ID_0 | TASSEL_1 |TACLR;
+                                TA0CTL &= ~TAIFG;
+                                TA0CTL |= TACLR;
+                                P1OUT ^=BIT0;
+                                break;
+                            }
+                            sendString("\n\rInvalid\n\r");
+                        }
+                    }
+                }
+                break;
+            case USCI_UART_UCTXIFG: break;
+            case USCI_UART_UCSTTIFG: break;
+            case USCI_UART_UCTXCPTIFG: break;
+        }
+
+}
+
+
